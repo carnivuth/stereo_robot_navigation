f175c8574a0d3722d74dbc5a5d01dd1420ffe5e7
carnivuth <matti200042@gmail.com>
2024-03-12 20:24:03 +0100

    added function for showing files

--- project.py
+++ project.py
@@ -2,26 +2,31 @@ import cv2 as cv
 #import numpy as np
 #import matplotlib.pyplot as plt
 
-import os
-os.environ["QT_QPA_PLATFORM_PLUGIN_PATH"] = "wayland"
 
+## LOAD VIDEOS
 LCameraView= cv.VideoCapture('robotL.avi' )
 RCameraView= cv.VideoCapture('robotR.avi')
  
-while LCameraView.isOpened() :
-    ret, frame = LCameraView.read()
- 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        print(ret)
-        break
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow("frame", gray) 
-    #cv.imshow('frame', gray)
-    if cv.waitKey(1) == ord('q'):
-        break
- 
-LCameraView.release()
-RCameraView.release()
-cv.destroyAllWindows()
+# SHOW VIDEO FRAMES
+def playVideos(LCameraView, RCameraView):
+    while LCameraView.isOpened() and RCameraView.isOpened():
+        Lret, Lframe = LCameraView.read()
+        Rret, Rframe = LCameraView.read()
+     
+        # if frame is read correctly ret is True
+        if not Lret or not Rret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+        Lgray = cv.cvtColor(Lframe, cv.COLOR_BGR2GRAY)
+        Rgray = cv.cvtColor(Rframe, cv.COLOR_BGR2GRAY)
+        cv.imshow("Lframe", Lgray) 
+        cv.imshow("Rframe", Rgray) 
+        #cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+     
+    LCameraView.release()
+    RCameraView.release()
+    cv.destroyAllWindows()
+
+playVideos(LCameraView,RCameraView)
