973e1e79a453c505c3525f0a8acd3a298dd3c446
carnivuth <matti200042@gmail.com>
2024-03-14 17:34:37 +0100

    added cli support for scripting and disparity map algorithm

--- project.py
+++ project.py
@@ -1,13 +1,65 @@
+import argparse
 import cv2 as cv
-#import numpy as np
-#import matplotlib.pyplot as plt
-
-
-## LOAD VIDEOS
-LCameraView= cv.VideoCapture('robotL.avi' )
-RCameraView= cv.VideoCapture('robotR.avi')
+import sys
+import matplotlib.pyplot as plt
  
-# SHOW VIDEO FRAMES
+# GET ARGUMENTS
+def getParams():
+    parser = argparse.ArgumentParser(prog='CVproject',description='computer vision project',epilog='credits carnivuth')
+    parser.add_argument('-d','--numDisparities',default='16',help='numDisparities parameter for disparity map algorithm')
+    parser.add_argument('-b','--blockSize',default='17',help='blocksize parameter for disparity map algorithm')
+    return parser.parse_args()
+
+# COMPUTE DISPARITY MAP
+def computeDisparityMap(LCameraView,RCameraView,numDisparities=16,blockSize=17):
+
+    try:
+        while LCameraView.isOpened() and RCameraView.isOpened():
+
+            # Extract frames
+            Lret, frameL = LCameraView.read()
+            Rret, frameR = RCameraView.read()
+
+            # Check for corrupted frames
+            if not Lret or frameL is None or  not Rret or frameR is None:
+                LCameraView.release()
+                RCameraView.release()
+                break
+                
+            # Calculate center frame
+            center = frameL.shape
+            centerY = int(center[0]/2)
+            centerX = int(center[1]/2)
+            interval = 50
+            
+            # Get image center box
+            imgL = cv.cvtColor(frameL, cv.COLOR_BGR2GRAY)#[centerY-interval:centerY+interval, centerX-interval:centerX+interval]
+            imgR = cv.cvtColor(frameR, cv.COLOR_BGR2GRAY)#[centerY-interval:centerY+interval, centerX-interval:centerX+interval]
+            
+            # Set Disparity map algotithm's parameters
+            stereoMatcher = cv.StereoBM_create()
+            stereoMatcher.setNumDisparities(numDisparities)
+            stereoMatcher.setBlockSize(blockSize)
+            #stereoMatcher.setSpeckleRange(16)
+            #stereoMatcher.setMinDisparity(0)
+            #stereoMatcher.setSpeckleWindowSize(45)
+            
+            # Disparity map computing
+            disparity = stereoMatcher.compute(imgL, imgR)
+            disparityImg = cv.normalize(disparity, disparity, alpha=255,beta=0, norm_type=cv.NORM_MINMAX, dtype=cv.CV_8UC1)
+            disparityImg = cv.applyColorMap(disparityImg, cv.COLORMAP_JET)
+            
+            plt.figure(1); plt.clf()
+            plt.imshow(disparityImg)
+            plt.title('disparity')
+            plt.pause(0.000001)
+
+    except KeyboardInterrupt:
+        LCameraView.release()
+        RCameraView.release()
+        print("Released Video Resource")
+
+# SHOW VIDEO FRAMES IN GRAYSCALE
 def playVideos(LCameraView, RCameraView):
     while LCameraView.isOpened() and RCameraView.isOpened():
         Lret, Lframe = LCameraView.read()
@@ -17,6 +69,7 @@ def playVideos(LCameraView, RCameraView):
         if not Lret or not Rret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
+
         Lgray = cv.cvtColor(Lframe, cv.COLOR_BGR2GRAY)
         Rgray = cv.cvtColor(Rframe, cv.COLOR_BGR2GRAY)
         cv.imshow("Lframe", Lgray) 
@@ -27,6 +80,12 @@ def playVideos(LCameraView, RCameraView):
      
     LCameraView.release()
     RCameraView.release()
-    cv.destroyAllWindows()
+#    cv.destroyAllWindows()
 
-playVideos(LCameraView,RCameraView)
+## LOAD VIDEOS
+LCameraView= cv.VideoCapture('robotL.avi')
+RCameraView= cv.VideoCapture('robotR.avi')
+#playVideos(LCameraView,RCameraView)
+args =getParams()
+computeDisparityMap(LCameraView,RCameraView,int(args.numDisparities),int(args.blockSize))
+
