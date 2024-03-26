9383ffb9b3cee869c6a6008b6f0150fabb67476f
carnivuth <matti200042@gmail.com>
2024-03-26 19:56:11 +0100

    fixed text

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-for nd in {16,32,64,128}; do
+for nd in {16,32,64}; do
   for bs in {25,30,50}; do
-    python project.py -d $nd -b $bs &
+    python project.py -d $nd -b $bs > stdout_$nd_$bs &
   done
 done
