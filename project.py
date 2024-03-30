48a1a75d164fec1bd3476f0e4b32489d9bd7e2bf
carnivuth <matti200042@gmail.com>
2024-03-30 14:09:09 +0100

    refactor in functions

--- project.py
+++ project.py
@@ -29,93 +29,36 @@ def imshow(wname,title, img):
 # GET ARGUMENTS
 def getParams():
     parser = argparse.ArgumentParser(prog='CVproject',description='computer vision project',epilog='credits carnivuth')
-    parser.add_argument('-d','--numDisparities',default='16',help='numDisparities parameter for disparity map algorithm')
-    parser.add_argument('-b','--blockSize',default='13',help='blocksize parameter for disparity map algorithm')
+    parser.add_argument('-d','--numDisparities',default='128',help='numDisparities parameter for disparity map algorithm',type=int)
+    parser.add_argument('-b','--blockSize',default='13',help='blocksize parameter for disparity map algorithm', type=int)
+    parser.add_argument('-c','--chessboard',help='Compute chessboard recognition',action='store_true')
     return parser.parse_args()
 
-# COMPUTE DISPARITY MAP
-def computeDisparityMap(LCameraView,RCameraView,numDisparities,blockSize):
+# FIND CHESBOARD IN THE IMAGE
+def computeChessboard(imgL,z):
+    ret,corners = cv.findChessboardCorners(imgL ,(CB_INNER_H_CORNERS,CB_INNER_W_CORNERS))
 
-    try:
-        while LCameraView.isOpened() and RCameraView.isOpened():
+    if ret == True:
 
-            # Extract frames
-            Lret, frameL = LCameraView.read()
-            Rret, frameR = RCameraView.read()
+        # draw chessboard corners in the left image
+        cv.drawChessboardCorners(imgL, (CB_INNER_H_CORNERS,CB_INNER_W_CORNERS), corners, ret)
+        imshow("chessboard",'chessboard',imgL)
 
-            # Check for corrupted frames
-            if not Lret or frameL is None or  not Rret or frameR is None:
-                LCameraView.release()
-                RCameraView.release()
-                break
-                
-            # Calculate center frame
-            center = frameL.shape
-            centerY = int(center[0]/2)
-            centerX = int(center[1]/2)
-            interval = 100
-            
-            # Get image frames
-            imgL = cv.cvtColor(frameL, cv.COLOR_BGR2GRAY)
-            imgR = cv.cvtColor(frameR, cv.COLOR_BGR2GRAY)
-            imgLCutted = imgL[centerY-interval:centerY+interval, centerX-interval:centerX+interval]
-            imgRCutted = imgR[centerY-interval:centerY+interval, centerX-interval:centerX+interval]
-            #imgL = imgLCutted
-            #imgR = imgRCutted
-            
-            # Set Disparity map algotithm's parameters
-            stereoMatcher = cv.StereoBM_create()
-            stereoMatcher.setNumDisparities(numDisparities)
-            stereoMatcher.setBlockSize(blockSize)
-            
-            # Disparity map computing
-            disparity = stereoMatcher.compute(imgLCutted, imgRCutted)
-            
-            # main disparity computing
-            dMain = np.absolute(disparity).mean()
+        # get w value WRONG VALUES
+        h =abs(corners[CB_INNER_H_CORNERS][0][1]- corners[0][0][1])
+        w =abs(corners[CB_INNER_H_CORNERS][0][0]- corners[corners.shape[0]-1][0][1])
 
-            # depth computing
-            z = (FOCAL_LENGHT * BASELINE)/dMain
+        # compute W value 
+        HComputed =z * h/FOCAL_LENGHT 
+        WComputed =z * w/FOCAL_LENGHT 
+    
+        # compare W value with real w of the chessboard
+        Hdiff = abs(HComputed- H)
+        Wdiff = abs(WComputed- W)
 
-            # printing disparity map
-            imshow("disparity",'disparity with numDisparity {} and blockSize {}'.format(numDisparities,blockSize),disparity)
-
-            # printing results
-            print("main disparity value is {}".format(dMain))
-            print("distance from the object is {} meters".format(z/1000))
-
-            if (z/1000 < MINIMUM_DISTANCE):
-                print("distance from the object under the minimum distance, detected distance: {} meters".format(z/1000))
-
-            # get chessboard corners
-            ret,corners = cv.findChessboardCorners(imgL ,(CB_INNER_H_CORNERS,CB_INNER_W_CORNERS))
-
-            if ret == True:
-
-                # draw chessboard corners in the left image
-                cv.drawChessboardCorners(imgL, (CB_INNER_H_CORNERS,CB_INNER_W_CORNERS), corners, ret)
-                imshow("chessboard",'chessboard',imgL)
-
-                # get w value WRONG VALUES
-                h =abs(corners[CB_INNER_H_CORNERS][0][1]- corners[0][0][1])
-                w =abs(corners[CB_INNER_H_CORNERS][0][0]- corners[corners.shape[0]-1][0][1])
-
-                # compute W value 
-                HComputed =z * h/FOCAL_LENGHT 
-                WComputed =z * w/FOCAL_LENGHT 
-                
-                # compare W value with real w of the chessboard
-                Hdiff = abs(HComputed- H)
-                Wdiff = abs(WComputed- W)
-
-                # print differences
-                print("The difference from the computed H and the real H is {}".format(Hdiff))
-                print("The difference from the computed W and the real W is {}".format(Wdiff))
-
-    except KeyboardInterrupt:
-        LCameraView.release()
-        RCameraView.release()
-        print("Released Video Resource")
+        # print differences
+        print("The difference from the computed H and the real H is {}".format(Hdiff))
+        print("The difference from the computed W and the real W is {}".format(Wdiff))
 
 # SHOW VIDEO FRAMES IN GRAYSCALE
 def playVideos(LCameraView, RCameraView):
@@ -138,6 +81,75 @@ def playVideos(LCameraView, RCameraView):
     LCameraView.release()
     RCameraView.release()
 
+def computeDisparityMap(imgL,imgR,frameShape,numDisparities,blockSize):
+
+    # Calculate center frame
+    center = frameShape
+    centerY = int(center[0]/2)
+    centerX = int(center[1]/2)
+    interval = 100
+    
+    imgLCutted = imgL[centerY-interval:centerY+interval, centerX-interval:centerX+interval]
+    imgRCutted = imgR[centerY-interval:centerY+interval, centerX-interval:centerX+interval]
+            
+    # Set Disparity map algotithm's parameters
+    stereoMatcher = cv.StereoBM_create()
+    stereoMatcher.setNumDisparities(numDisparities)
+    stereoMatcher.setBlockSize(blockSize)
+    
+    # Disparity map computing
+    disparity = stereoMatcher.compute(imgLCutted, imgRCutted)
+            
+    # main disparity computing
+    dMain = np.absolute(disparity).mean()
+
+    # depth computing
+    z = (FOCAL_LENGHT * BASELINE)/dMain
+
+    # printing disparity map
+    imshow("disparity",'disparity with numDisparity {} and blockSize {}'.format(numDisparities,blockSize),disparity)
+
+    # printing results
+    print("main disparity value is {}".format(dMain))
+    print("distance from the object is {} meters".format(z/1000))
+
+    if (z/1000 < MINIMUM_DISTANCE):
+        print("distance from the object under the minimum distance, detected distance: {} meters".format(z/1000))
+
+    return z
+
+def main(LCameraView,RCameraView,numDisparities,blockSize,chessboard):
+
+    try:
+        while LCameraView.isOpened() and RCameraView.isOpened():
+
+            # Extract frames
+            Lret, frameL = LCameraView.read()
+            Rret, frameR = RCameraView.read()
+
+            # Check for corrupted frames
+            if not Lret or frameL is None or  not Rret or frameR is None:
+                LCameraView.release()
+                RCameraView.release()
+                break
+                
+            # Get image frames
+            imgL = cv.cvtColor(frameL, cv.COLOR_BGR2GRAY)
+            imgR = cv.cvtColor(frameR, cv.COLOR_BGR2GRAY)
+
+            z=computeDisparityMap(imgL,imgR,frameL.shape,numDisparities,blockSize)
+
+            if chessboard: 
+                computeChessboard(imgL,z)
+
+
+
+    except KeyboardInterrupt:
+        LCameraView.release()
+        RCameraView.release()
+        print("Released Video Resource")
+
+
 # LOAD VIDEOS
 LCameraView= cv.VideoCapture('robotL.avi')
 RCameraView= cv.VideoCapture('robotR.avi')
@@ -145,6 +157,6 @@ RCameraView= cv.VideoCapture('robotR.avi')
 # READ PARAMS
 args =getParams()
 
-# COMPUTE DISPARITY MAP
-computeDisparityMap(LCameraView,RCameraView,int(args.numDisparities),int(args.blockSize))
+# MAIN COMPUTATION
+main(LCameraView,RCameraView,args.numDisparities,args.blockSize,args.chessboard)
 
