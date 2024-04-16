8d5a435d6df374ff93f2d477e215825028647248
carnivuth <matti200042@gmail.com>
2024-04-16 11:18:38 +0200

    added constants

---
+++ constants.py
@@ -0,0 +1,17 @@
+# chessboard dimensions (in millimeters)
+H = 178
+W = 125
+
+# chessboard parameters
+CB_INNER_W_CORNERS = 6
+CB_INNER_H_CORNERS = 8
+
+# best blocksize value 
+BEST_BLOCKSIZE_VALUE=33
+
+# allarm trigger limit (in meters)
+MINIMUM_DISTANCE =  0.8
+
+# camera parameters
+FOCAL_LENGHT =  567.2 
+BASELINE = 92.226 
